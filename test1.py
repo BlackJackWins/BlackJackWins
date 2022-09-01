de88aba2e9a9b02157e6659768304c382b88dfa4
BlackJackWins <69159120+BlackJackWins@users.noreply.github.com>
2022-09-01 18:10:48 -0300

    Add files via upload

--- test1.py
+++ test1.py
@@ -2,3 +2,6 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.set_page_config(page_title="NEW App",
+                    page_icon = ":bar_chart:",
+                    layout)
