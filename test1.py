f972fcc6675c13ea985d7d63bb129800927ca36b
BlackJackWins <69159120+BlackJackWins@users.noreply.github.com>
2022-09-02 19:17:29 -0300

    Add files via upload

--- test1.py
+++ test1.py
@@ -2,6 +2,20 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.set_page_config(page_title="NEW App",
+st.set_page_config(page_title="NEW Javier App",
                     page_icon = ":bar_chart:",
                     layout="wide")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
